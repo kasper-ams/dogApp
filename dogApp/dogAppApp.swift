03256968e4d658eb58d75f6f63d3cfd7762b23d9
Kaspars <kaspars.jelaiskis@printify.com>
2023-08-30 18:08:58 +0300

    Initial Commit

---
+++ dogApp/dogAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  dogAppApp.swift
+//  dogApp
+//
+//  Created by Kaspars Jelaiskis on 30/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct dogAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
